0b9419a69a77c4e578de0378b8a84d492f3ae31c
richasrichasrichas <abdalla.ricardo0606@gmail.com>
2026-08-04 14:23:18 -0300

    Mini project 3 - Goalkeeper text choice game

---
+++ 3 - choice text game/mini project 3 - choice text game.py
@@ -0,0 +1,76 @@
+print('Welcome to')
+print('''                  _ _                             
+                  | | |                            
+  __ _  ___   __ _| | | _____  ___ _ __   ___ _ __ 
+ / _` |/ _ \ / _` | | |/ / _ \/ _ \ '_ \ / _ \ '__|
+| (_| | (_) | (_| | |   <  __/  __/ |_) |  __/ |   
+ \__, |\___/ \__,_|_|_|\_\___|\___| .__/ \___|_|   
+  __/ |                           | |              
+ |___/                            |_|         ''')
+
+input('Press enter to start.')
+
+print('Your name is Marcus Iverson, an English Championship goalkeeper at the Merseyside Ogres F.C')
+input('''                  __,='`````'=/__
+                          '//  (o) \(o) \ `'         _,-,
+                          //|     ,_)   (`\      ,-'`_,-\
+                        ,-~~~\  `'==='  /-,      \==```` \__
+                       /        `----'     `\     \       \/
+                    ,-`                  ,   \  ,.-\       \
+                   /      ,               \,-`\`_,-`\_,..--'\
+                  ,`    ,/,              ,>,   )     \--`````\
+                  (      `\`---'`  `-,-'`_,<   \      \_,.--'`
+                   `.      `--. _,-'`_,-`  |    \
+                    [`-.___   <`_,-'`------(    /
+                    (`` _,-\   \ --`````````|--`
+                     >-`_,-`\,-` ,          |
+                   <`_,'     ,  /\          /
+                    `  \/\,-/ `/  \/`\_/V\_/
+                       (  ._. )    ( .__. )
+                       |      |    |      |
+                        \,---_|    |_---./
+                        ooOO(_)    (_)OOoo''')
+print('After you got relegated, this is the chance for you to rise up to the Premier League again with your team.')
+input('''  _______
+          | _     |
+    o     |_o=--  |
+   () o
+   |\,
+''')
+print('The match stands 1-0 to your club against your rivals, the Droylsden Butchers, and this is the last strike of the game.')
+print('''     ____________        _..._
+     | ________ |  ___ .'     `. ____________
+     ||        || |   /`.  .-'  \            |
+     ||        || |   )X / X.   "\           |
+     ||        || |   | (_)     _/           |
+     ||________|| |   /((())) / \_____       |
+     |        ] | |   \______/  //    \      |
+     |          | |   _..-)    //  (\/)\     |
+     |          | |  / .\\_..-' \   \/  \    |
+     |          | |_/    \       `. Mom  \___|
+     |          |  /     |         \     /
+_____|__________| /      /         /    / _______
+                  |    ./|        /    /
+                 _|____\_|        |   /
+                 \       /       _)   \__
+                  \     /       (_\    \()
+               ___|_____|____  / / \\_\/
+            __/           |  |/ /___|
+    ..'`..'|  |           | _/_/    |       
+  .'(( ((( |__|           |(__()    |
+.'((( ((( .'__|           |  |______|________
+ ((( (( ((`.  |           |  |              /
+(((((  (((((`.|           |  |             /
+  ((( ((( ((.'|           | /             /
+`..-''-....'  |___________|/             /
+
+''')
+input('Can you save the game from this shot?')
+print('The striker sets it and shoots.')
+print('''         
+     -   \O                                     ,  .-.___
+   -     /\                                   O/  /xx\XXX\
+  -   __/\ `                                  /\  |xx|XXX|
+     `    \, ()                              ` << |xx|XXX|
+ jgs^^^^^^^^`^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^''')
+input('Which side do you go to block the ball? Left-Up Left-Down Right-Up Right-Down')
