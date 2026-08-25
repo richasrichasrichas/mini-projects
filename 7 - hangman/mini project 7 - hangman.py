fa3f29facf7628b56aad8b95b50d14ddebeadbd2
richasrichasrichas <abdalla.ricardo0606@gmail.com>
2026-08-25 08:59:16 -0300

    Add csvfile with portuguese dictionary

--- 7 - hangman/mini project 7 - hangman.py
+++ 7 - hangman/mini project 7 - hangman.py
@@ -2,6 +2,10 @@
 import random
 import pandas
 
-lista = [open(str('C:\\Users\\ricar\\OneDrive\\Desktop\\mini projects\\7 - hangman\\assets\\br-sem-acentos.txt'))]
-
-print(lista)
+# ABRE O ARQUIVO DA LISTA DO DICIONÁRIO
+def nome_aleatório():
+    import csv
+    with open('br-sem-acentos.csv', newline = '') as csvfile:
+        spamreader = csv.reader('br-sem-acentos.csv', delimiter = ' ', quotechar = '|')
+        for row in spamreader:
+            print(', '.join(row))
