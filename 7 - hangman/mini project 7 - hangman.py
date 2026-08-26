c754799c7c29e871f1657178e460507bf34d86ce
richasrichasrichas <abdalla.ricardo0606@gmail.com>
2026-08-26 19:05:24 -0300

    complete todo 1

--- 7 - hangman/mini project 7 - hangman.py
+++ 7 - hangman/mini project 7 - hangman.py
@@ -3,12 +3,12 @@ import random
 import pathlib
 
 # ABRE O ARQUIVO DA LISTA DO DICIONÁRIO
-def nome_aleatório():
+def nome_aleatorio():
     import csv
-    with open(r'C:\Users\ricar\OneDrive\Desktop\mini projects\7 - hangman\br-sem-acentos.csv'):
-        reader = csv.reader('br-sem-acentos.csv', delimiter = " ", quotechar = '|')
-        data = list(reader)
-        for column in reader:
-            random.choice(br-sem-acentos.csv)
+    caminho = r'C:\Users\ricar\OneDrive\Desktop\mini projects\7 - hangman\br-sem-acentos.csv'
+    with open(caminho, encoding='utf-8') as arquivo:
+        leitor = csv.reader(arquivo)  # quotechar='"' já é o padrão
+        palavras = [linha[0] for linha in leitor if linha]
+    return random.choice(palavras)
 
-print(nome_aleatório())
+print(nome_aleatorio())
