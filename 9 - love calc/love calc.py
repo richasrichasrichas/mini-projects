6bb74f8416ee06b46400be61a20eaf0e1cefba90
richasrichasrichas <abdalla.ricardo0606@gmail.com>
2026-08-28 13:51:40 -0300

    love calc, probably wrong

---
+++ 9 - love calc/love calc.py
@@ -0,0 +1,7 @@
+def calculate_love_score(name1, name2):
+    names = (name1 + name2).upper()
+    true_l = sum(names.count(letter) for letter in 'TRUE')
+    t_love = sum(names.count(letter) for letter in 'LOVE')
+    return print(f'{true_l}{t_love}')
+    
+calculate_love_score('Kanye West', 'Kim Kardashian')
